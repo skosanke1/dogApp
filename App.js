33264bb68ac0238e2dab2e67cfc6c433e0e7b732
Steven Kosanke <skosanke@asu.edu>
2023-05-08 14:23:12 -0600

    Add fetch button and displays random dogs

--- App.js
+++ App.js
@@ -1,10 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Image, Button, Dimensions, StatusBar, ActivityIndicator } from 'react-native';
 
 export default function App() {
+  const [imageUrl, setImageUrl] = useState(null);
+  const [imageSize, setImageSize] = useState({ width: 0, height: 0 });
+  const [isLoading, setIsLoading] = useState(false);
+
+  const fetchImage = () => {
+    setIsLoading(true);
+    fetch('https://random.dog/woof.json')
+      .then(response => response.json())
+      .then(data => {
+        if (data.url.endsWith('.mp4') || data.url.endsWith('.webm')) {
+          fetchImage();
+        } else {
+          setImageUrl(data.url);
+          Image.getSize(data.url, (width, height) => {
+            const deviceWidth = Dimensions.get('window').width;
+            const scaleFactor = width / deviceWidth;
+            const imageHeight = height / scaleFactor;
+            setImageSize({ width: deviceWidth, height: imageHeight });
+            setIsLoading(false);
+          });
+        }
+      });
+  };
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.title}>Dog App</Text>
+      {isLoading ? (
+        <ActivityIndicator size="large" />
+      ) : (
+        imageUrl && <Image source={{ uri: imageUrl }} style={imageSize} />
+      )}
+      <View style={styles.buttonContainer}>
+        <Button title="Refresh Image" onPress={fetchImage} />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +47,21 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    alignSelf: 'center',
+    position: 'absolute',
+    top: '5%',
+  },
+  buttonContainer: {
+    justifyContent: 'space-between',
+    width: '80%',
+    marginTop: 20,
+    alignSelf: 'center',
+    position: 'absolute',
+    bottom: '5%',
   },
 });
